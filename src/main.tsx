3523e4066ef156ecf1c2f70d1f2ab3287f7245b1
iAmShivay <shivaysharma77894@gmail.com>
2024-12-06 11:41:20 +0530

    refactor: remove unused ErrorSnackbar component import from main rendering

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import store from "./store.tsx";
 import Snackbar from "./components/errorSnackbar.tsx";
 import MinimalistHRLoader from "./pages/Loading.tsx";
-import ErrorSnackbar from "./components/globalerror.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
